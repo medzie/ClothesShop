92af7a11b4c4c75e65721633c4fad1f5fdd85b39
Samartsava11 <79637094+Samartsava11@users.noreply.github.com>
2021-11-23 14:37:21 +0100

    SomethingNew
    szyfrowanie haseł

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -1,7 +1,6 @@
 public class Worker {
 
     // zmiana ceny, artykułów, nazw, ilości
-    // Lajza jest dziwna
-    //nie jestem dziwna
+
 
 }
--- src/main/java/WorkerAccount.java
+++ src/main/java/WorkerAccount.java
@@ -1,5 +1,5 @@
 public class WorkerAccount {
 
     // logowanie
-
+    //szyfrowanie haseł
 }
